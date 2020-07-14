325566c222f3d5fd87509ba3e986407eaf2676d6
cathjime <xcatherine.jimenez@gmail.com>
2020-07-14 09:56:37 -0400

    deliverable 3 pending

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/***** Beginning of Starter Code ****/
+// /***** Beginning of Starter Code ****/
 
 const playerContainer = document.querySelector(".player-container")
 
@@ -26,12 +26,14 @@ function renderPlayer(player) {
 // for each player in the array, render to the DOM
 PLAYERS.forEach(renderPlayer)
 
-/***** End of Starter Code ****/
+// /***** End of Starter Code ****/
 
 
 
 
 /***** Deliverable 1 *****/
+
+
 function toggleColor(element) {
   if (element.style.color === "red") {
     element.style.color = "black"
@@ -40,7 +42,43 @@ function toggleColor(element) {
   }
 }
 
+const header = document.getElementById("header")
+header.addEventListener("click", function(e){
+    toggleColor(header)
+});
+
+
 
 /***** Deliverable 2 *****/
+const playerForm = document.getElementById("new-player-form")
+
+playerForm.addEventListener("submit", function(e){
+  e.preventDefault()
+
+ // console.log(e.target.number)
+
+  let newPlayer =  {
+    jersey: e.target.number.value,
+    name: e.target.name.value,
+    nickname: e.target.nickname.value,
+    photo: e.target.photo.value,
+    likes: 0
+  }
+
+  renderPlayer(newPlayer)
+})
+
 
 /***** Deliverable 3 *****/
+//When a user clicks the like button for a player, 
+//that player's likes should increase by 1.
+playerContainer.addEventListener("click", function(e){
+  const likeBtn = playerContainer.querySelector(".like-button")
+  let likes = playerContainer.querySelector(".likes")
+  let newLike = parseInt(likes.innerHTML.split(" ")[0])
+  // console.log(newLike)
+    if(e.target === likeBtn){
+      newLike += 1
+      likes = newLike + " likes"
+    }
+})
